f5322d65ccd50c3890a476d9336299c046250179
Altorus <101462143+Altorus@users.noreply.github.com>
2022-04-06 23:01:38 +0400

    task 6

---
+++ js/script.js
@@ -0,0 +1,82 @@
+//1
+// message = prompt("Кто вошёл?");
+// if (message =='Админ') {
+//     pass = prompt("Введите пароль");
+//     if (pass =='Чёрный властелин') {
+//         alert("Добро пожаловать")
+//     }
+//     else if (message != 'Чёрный властелин') {
+//         alert("Пароль не верный")
+//     }
+//     else{
+//         alert("Вход отменен")
+//     }
+// }
+// else if (message != 'Админ') {
+//     alert("Я вас не знаю")
+// }
+// else{
+//     alert("Вход отменен")
+// }
+
+// 2
+// str='abcde'
+// alert(str[0])
+// alert(str[1])
+// alert(str[4])
+
+//3
+// hour=prompt('Введите кол-во часов')
+// alert(hour+' часов это '+ hour*3600 + " секунд")
+
+//4
+// num=1
+// num+=12
+// num-=14
+// num*=5
+// num/=7
+// num+=1
+// num-=1
+// alert(num)
+
+//5
+// num=3
+// alert(num)
+
+// a=10
+// b=2
+// alert(a+b)
+// alert(a-b)
+// alert(a*b)
+// alert(a/b)
+
+// c=15
+// d=2
+// result=c+в
+// alert(result)
+
+// a=10
+// b=2
+// c=5
+// alert(a+b+c)
+
+// a=17
+// b=10
+// c=a-b
+// d=7
+// result=c+d
+// alert(result)
+
+//6
+// let str = "Привет мир!"
+// alert(str)
+
+// let str1 = "Привет "
+// let str2 = "мир!"
+// alert(str1+str2)
+
+// let name="Ваня"
+// alert('Привет, '+ name +'!')
+
+// let age = 18;
+// alert('Мой возраст, '+ age +'!')
